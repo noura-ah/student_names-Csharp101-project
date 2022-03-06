b7a0eb63eada3bb3266e4f0b17908f1cc5fc0e39
noura-ah <noura.ah1493@gmail.com>
2022-03-07 02:02:46 +0300

    update all code

--- student_names/Program.cs
+++ student_names/Program.cs
@@ -11,10 +11,11 @@ namespace ConsoleApp1
 
         public static void Main(string[] args)
         {
-            string path = @"MyTest.txt";
-            string arr;
+            file new_file = new file();
+            new_file.path = @"student_name_list.txt";
 
-            //to switch between add, update ,print  and finish 
+            student student = new student();
+            
             string c = "";
 
             while (c != "f")
@@ -24,81 +25,76 @@ namespace ConsoleApp1
 
                 switch (c)
                 {
-                    case "a":
-                        arr = enter_student();
-
-                        if (!File.Exists(path)) using (StreamWriter sw = File.CreateText(path)) write_to_file(path, arr, sw);
-                        else using (StreamWriter sw = File.AppendText(path)) write_to_file(path, arr, sw);
-
+                    case "a"://add student name
+                        new_file.addName(student);
                         break;
 
-                    case "p":
-
-                        if (file_dont_exist_prnt(path)) break;
-
-                        string s = "";
-                        using (StreamReader sr = File.OpenText(path)) while ((s = sr.ReadLine()) != null) Console.WriteLine(s);
-
+                    case "p"://print students list
+                        if (new_file.file_not_exist()) break;
+                        new_file.printList();
                         break;
 
-                    case "u":
-
-                        if (file_dont_exist_prnt(path)) break;
-                        arr = enter_student();
-                        var values = arr.Split(',');
-
-                        List<string> quotelist = File.ReadAllLines(path).ToList();
-
-                        int query = quotelist.FindIndex(x => x.Contains(values[0]));
-                        Console.WriteLine(query + "line");
-
-                        if (query == -1)
-                        {
-                            Console.WriteLine(values[0] + " is not in the list");
-                            break;
-                        }
-
-                        string pass_1 = "passed";
-                        if (Convert.ToInt32(values[1]) < 60) pass_1 = "failed";
-                        quotelist[query] = arr + "," + pass_1;
-                        File.WriteAllLines(path, quotelist.ToArray());
-
+                    case "u"://update student grade
+                        if (new_file.file_not_exist()) break;
+                        new_file.upadateName(student);
                         break;
-                    case "f":
+
+                    case "f"://end the program
                         break;
+
                     default:
                         Console.WriteLine("wrong letter, please enter again");
                         break;
 
                 }
             }
-
-
-
         }
 
-        public static string enter_student()
+       
+    }
+    class student
+    {
+        public string name; 
+        public int grade;
+        public string pass;
+
+        public student()
+        {
+            name = "ahmed";
+            grade = 100;
+            pass = "passed";
+        }
+        public void readName()
         {
             Console.WriteLine("Enter name:");
-            string name = Console.ReadLine();
+            name = Console.ReadLine();
             Console.WriteLine("Enter grade:");
-            string grade = Console.ReadLine();
-
-            List<string> myValues = new List<string>() { name, grade };
-            string csv = String.Join(",", myValues.Select(x => x.ToString()).ToArray());
-
-            return csv;
+            grade = int.Parse(Console.ReadLine());
         }
 
-        public static void write_to_file(string path, string arr, StreamWriter sw)
+        
+        public string ispass()
         {
-            var items = arr.Split(',');
-            string pass_1 = "passed";
-            if (Convert.ToInt32(items[1]) < 60) pass_1 = "failed";
-            sw.Write(arr + "," + pass_1 + "\r\n");
+            if (grade < 60) pass = "failed";
+            else pass = "passed";
+            return pass;
+        }
+        public string[] returnName()
+        {
+            pass = ispass();
+            string grade_str=grade.ToString();
+            string[] arr = { name, grade_str, pass };
+            return arr;
+
         }
 
-        public static bool file_dont_exist_prnt(string path)
+    }
+    class file
+    {
+        
+        public string path;
+        
+        public bool file_not_exist()
         {
             if (!File.Exists(path))
             {
@@ -107,5 +103,58 @@ namespace ConsoleApp1
             }
             return false;
         }
-    }
+
+        public string printName(student s)
+        {
+            return String.Join(",", s.returnName().Select(x => x.ToString()));   
+        }
+
+        public void addName(student s)
+        {
+            if (!File.Exists(path)) using (StreamWriter sw = File.CreateText(path)) writeToFile(s, sw);
+            else using (StreamWriter sw = File.AppendText(path)) writeToFile(s, sw);
+        }
+        
+        public void writeToFile(student s, StreamWriter sw)
+        {
+            s.readName();
+            sw.Write(printName(s) + "\r\n");
+        }
+
+        public List<string> readFile()
+        {
+            return File.ReadAllLines(path).ToList();
+        }
+
+        public void printList()
+        {
+             List<string> list = readFile();
+             for (int i = 0; i < list.Count; i++)
+             Console.WriteLine(list[i]);
+        }
+        public void upadateName(student s)
+        {
+            s.readName();
+            var item = nameExist(s);
+            if (item.exist) 
+            {
+                item.quotelist[item.quary] = printName(s);
+                File.WriteAllLines(path, item.quotelist.ToArray());
+            }
+            else Console.WriteLine(s.name + " is not in the list");
+
+        }
+
+        public (bool exist,int quary,List<string> quotelist) nameExist(student s)
+        {
+            List<string> quotelist = readFile();
+            int query = quotelist.FindIndex(x => x.ToString().Split(",").Contains(s.name));
+        
+            if (query == -1) return (false, query, quotelist);
+
+            return (true, query, quotelist);
+        }
+    } 
+        
 }
+
